88b9995ce0472fb71063e196255cd1864da2128b
RafiS5 <rafi.subhan365@gmail.com>
2026-01-17 01:42:04 +0000

    Installed tailwind and fixed a few bugs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,45 +1,42 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from 'path';
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base:
-        mode == "development"
-            ? "http://localhost:5173/"
-            : "/static/api/spa/",
-    build: {
-        emptyOutDir: true,
-        outDir: "../api/static/api/spa",
+  base: mode == "development" ? "http://localhost:5173/" : "/static/api/spa/",
+  build: {
+    emptyOutDir: true,
+    outDir: "../api/static/api/spa",
+  },
+  plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-        },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        cookieDomainRewrite: "localhost",
+      },
+      "/admin": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        cookieDomainRewrite: "localhost",
+      },
+      "/accounts": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        cookieDomainRewrite: "localhost",
+      },
+      "/static": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        cookieDomainRewrite: "localhost",
+      },
     },
-    server: {
-        proxy: {
-            "/api": {
-                target: "http://localhost:8000",
-                changeOrigin: true,
-                cookieDomainRewrite: "localhost",
-            },
-            "/admin": {
-                target: "http://localhost:8000",
-                changeOrigin: true,
-                cookieDomainRewrite: "localhost",
-            },
-            "/accounts": {
-                target: "http://localhost:8000",
-                changeOrigin: true,
-                cookieDomainRewrite: "localhost",
-            },
-            "/static": {
-                target: "http://localhost:8000",
-                changeOrigin: true,
-                cookieDomainRewrite: "localhost",
-            },
-        },
-    },
+  },
 }));
