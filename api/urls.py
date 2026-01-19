71cc3dbdad565c8e345568a9cc035298d2c31c86
Evx <154090784+ADjehiche@users.noreply.github.com>
2026-01-19 11:03:52 +0000

    Revert "Issue 10 new item page"

--- api/urls.py
+++ api/urls.py
@@ -17,12 +17,9 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.http import HttpResponse
-from . import views
+
 from .views import main_spa
 
 urlpatterns = [
-    path("api/items/", views.items_collection, name="items_collection"),
-    path("api/logout/", views.api_logout, name="api_logout"),
-    path("api/auth/status/", views.auth_status, name="auth_status"),
-    path("", views.main_spa, name="main_spa"),
+    path('', main_spa),
 ]
--- project/urls.py
+++ project/urls.py
@@ -13,25 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.http import HttpResponse
-from api import views as api_views
-from django.conf import settings
-from django.conf.urls.static import static
 
-
-def health_check(_request):
-    return HttpResponse("OK")
 
 urlpatterns = [
-    path("health", health_check),
-    path("admin/", admin.site.urls),
-    path("accounts/signup/", api_views.signup, name="signup"),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("", include("api.urls")),
+    path('', include('api.urls')),
+    path('health', lambda request: HttpResponse("OK")),
+    path('admin/', admin.site.urls),
 ]
-
-# Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
