5ff0c331821295b63cfff79f17e0562c8aeaf41d
yasiralzaid <ec23981@qmul.ac.uk>
2026-01-19 15:00:00 +0300

    reconfigured custom user, media

--- project/settings.py
+++ project/settings.py
@@ -212,3 +212,9 @@ if DEBUG:
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 LOGIN_URL = "/accounts/login/"
+
+# Switch auth user model
+AUTH_USER_MODEL = "api.User"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
